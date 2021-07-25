3c151526d2d5e917218b4d8de16e255b03b05651
hendy_lazuardi <mhendylazuardi@gmail.com>
2021-07-25 21:44:37 +0700

    feat(update): css

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -25,7 +25,7 @@ export default function Home() {
             <img alt="myself" src={MySelf} height="300" />
           </Col>
           <Col className="text-white m-auto" md={8}>
-            <h1 className="">Mochammad Hendy Lazuardi</h1>
+            <h1 className="my-name">Mochammad Hendy Lazuardi</h1>
             <p className="about-me">Hi everyone, introduce my self Hendy, Frontend Developer from D.I. Yogyakarta, Indonesia.
               I already 3 years in Frontend Developer. I was graduated from University Amikom Yogyakarta in 2018 with GPA 3.49. I already
               create project such as B2B and B2C with payment gateway. Click button bellow to see what i already create!</p>
